bfdf833a044f905bfd5689c2a9bf5d833807e3bd
iskender=aman <amanibraev95@gmail.com>
2022-05-04 12:18:29 +0600

    routes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from "./views/Home/Home";
 import Favorites from "./views/Favorites/Favorites";
 import Detail from "./views/Detail/Detail";
 import Header from "./components/Header/Header";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 import * as PropTypes from "prop-types";
 
 BrowserRouter.propTypes = {children: PropTypes.node};
@@ -13,12 +13,14 @@ function App() {
   return (
     <div className="App">
         <BrowserRouter>
-            <Header/>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/favorites" element={<Favorites/>}/>
-                <Route path="/movie/:movie_id" element={<Detail/>}/>
-            </Routes>
+            <HashRouter basename="/">
+                <Header/>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/favorites" element={<Favorites/>}/>
+                    <Route path="/movie/:movie_id" element={<Detail/>}/>
+                </Routes>
+            </HashRouter>
         </BrowserRouter>
     </div>
   );
